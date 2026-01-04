d6838e276b099d04e9d479b89a3a7928a3d85980
Ayushman Gupta <125189298+guptaayushman24@users.noreply.github.com>
2026-01-04 09:40:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your JS code here. If required.
+const progress = document.querySelector('.progress');
+const circles = document.querySelectorAll('.circle');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let currentActive = 1;
+
+// Next button
+next.addEventListener('click', () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update();
+});
+
+// Previous button
+prev.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.circle.active');
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+  // Disable buttons at limits
+  prev.disabled = currentActive === 1;
+  next.disabled = currentActive === circles.length;
+}
